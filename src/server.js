d4c936370e44cc74bbaffd31026bd87cf3022a02
Tronghuong3110 <tronghuong2002@gmail.com>
2023-07-15 00:40:09 +0700

    config

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 const app = express()
-const port = 3000
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
 configViewEngine(app);
+
 
 app.get('/', (req, res) => {
   res.render("index.ejs");
